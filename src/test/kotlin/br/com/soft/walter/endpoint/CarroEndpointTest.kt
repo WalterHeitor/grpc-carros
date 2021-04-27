b956b29488bc077173ad82031a6d6aeaf3dd6763
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-27 15:22:12 -0300

    Create CarroEndpointTest.kt

---
+++ src/test/kotlin/br/com/soft/walter/endpoint/CarroEndpointTest.kt
@@ -0,0 +1,19 @@
+package br.com.soft.walter.endpoint
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+internal class CarroEndpointTest{
+
+    /**
+     * 1. happy path cadastro
+     * 2. quando ja existe um acaro com a placa.
+     * 3. quando os dados de entrada sao invalidos.
+     */
+    @Test
+    fun `deve cadastrar carro`(){
+
+    }
+}
