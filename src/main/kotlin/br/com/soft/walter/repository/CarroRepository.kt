50f0140648f9e4ad8da58e88ceac94e8219a4cd7
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-04-27 15:22:20 -0300

    Update CarroRepository.kt

--- src/main/kotlin/br/com/soft/walter/repository/CarroRepository.kt
+++ src/main/kotlin/br/com/soft/walter/repository/CarroRepository.kt
@@ -1,8 +1,9 @@
 package br.com.soft.walter.repository
 
 import br.com.soft.walter.cadastro.doumaim.Carro
+import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
-
+@Repository
 interface CarroRepository : JpaRepository<Carro, Long> {
     fun existsByPlaca(placa: String?): Boolean
 }
